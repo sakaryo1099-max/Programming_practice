0870d17ab79c738fbc803c11c50b06f547319d28
sakaryo1099-max <saka.ryo1099@gmail.com>
2026-06-25 23:54:50 +0900

    Atcoder

--- C/AtCoderBeginnersSelection/SomeSums.c
+++ C/AtCoderBeginnersSelection/SomeSums.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 
+int calc(int n){
+    int res = 0;
+    while(n > 0){
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
 int main(){
     int N, A, B;
 
     scanf("%d %d %d", &N, &A, &B);
 
-    for(int i = 1, i <= N; i++){
-        
+    int res = 0;
+    for(int n = 1; n <= N; n++){
+        int sum = calc(n);
+        if(sum >= A && sum <= B){
+            res += n;
+        }
     }
 
+    printf("%d", res);
 }
+
+
+
+// int sum = 1234;
+// int i;
+// unsigned char suuti[4];
+// for(i = 0; i < 4; i++){
+// suuti[i] = num % 10;
+// num = num / 10;
+// if(num == 0) break;
+// }
