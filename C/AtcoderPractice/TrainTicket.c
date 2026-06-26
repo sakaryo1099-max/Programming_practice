8a589b1864a9d0228d60814fce1fab8b36560009
sakaryo1099-max <saka.ryo1099@gmail.com>
2026-06-26 17:16:00 +0900

    Atcoder

---
+++ C/AtcoderPractice/TrainTicket.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void){
+    char S[5];
+
+    scanf("%s", S);
+
+    for(int mask = 0; mask < (1 << 3); mask++){
+        int sum = S[0] - '0';
+
+        for(int i = 0; i < 3; i++){
+            if(mask & (1 << i)){
+                sum += S[i + 1] - '0';
+            }else{
+                sum -= S[i + 1] - '0';
+            }
+        }
+
+        if(sum == 7){
+            printf("%c", S[0]);
+
+            for(int i = 0; i < 3; i++){
+                if(mask & (1 << i)){
+                    printf("+");
+                }else{
+                    printf("-");
+                }
+
+                printf("%c", S[i + 1]);
+            }
+
+            printf("=7\n");
+        }
+    }
+
+    return 0;
+}
